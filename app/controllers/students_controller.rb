0e5da0cb0d1b6aaafe4c92cb9cb73c23e561ae8c
Emery Tate <tateejt@gmail.com>
2020-01-14 06:03:38 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -18,7 +18,7 @@ class StudentsController < ApplicationController
 
   def show
     @student = Student.find(params[:id])
-    @classroom = @student.
+    @classroom = @student.classroom
   end
 
   def index
